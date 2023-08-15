87c8c0a0dc8c67c214163df0306d0570d85cd18c
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-15 11:52:15 +0300

    add

---
+++ conftest.py
@@ -0,0 +1,11 @@
+# Конфигурация тестов
+import pytest                   # пакет pytest
+from selenium import webdriver  # пакет Вебдрайвер
+
+# фикстура запуска/закрытия браузера
+@pytest.fixture(scope='function')
+def browser():
+    browser = webdriver.Chrome()  # инициализируем драйвер браузера
+    browser.implicitly_wait(10)   # ждать 10 сек. до поиска элемента
+    yield browser                 # финализатор
+    browser.quit()                # закрыть браузер
