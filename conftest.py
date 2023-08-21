be5c2b1129f0d9c1cba865ba4f5c3d7111eeede2
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-21 15:11:33 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,6 +6,5 @@ from selenium import webdriver  # пакет Вебдрайвер
 @pytest.fixture(scope='function')
 def browser():
     browser = webdriver.Chrome()  # инициализируем драйвер браузера
-    browser.implicitly_wait(10)   # ждать 10 сек. до поиска элемента
     yield browser                 # финализатор
     browser.quit()                # закрыть браузер
