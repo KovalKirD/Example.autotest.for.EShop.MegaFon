f9a61bc80058358f381098c257009fa9862f1452
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-24 16:47:33 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
 class MainPage(BasePage):
-    def select_tarif_on_main(self):
-        # ВЫБРАТЬ ТАРИФ И ДОБАВИТЬ В КОРЗИНУ
+    def click_button_buy_megatarif(self):  # Нажать кнопку "Купить" на МегаТариф
         button_buy_megatarif = self.browser.find_element(*MainPageLocators.BUTTON_BUY_MEGATARIF)
-        button_buy_megatarif.click()  # Нажать кнопку "Купить" на тарифе
+        button_buy_megatarif.click()
 
+    def click_button_buy_sim(self):  # Нажать кнопку "Купить SIM"
         button_buy_sim = self.browser.find_element(*MainPageLocators.BUTTON_BUY_SIM_OR_ESIM)
-        button_buy_sim.click()  # Нажать кнопку "Купить SIM"
+        button_buy_sim.click()
