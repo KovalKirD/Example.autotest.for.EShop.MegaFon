b63ac891efc073c77357e6cb4396a52833b19b65
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-29 16:50:14 +0300

    create locators

--- pages/locators.py
+++ pages/locators.py
@@ -19,8 +19,16 @@ class BasketPageLocators:
     BUTTON_SELECT_ADDRESS_DELIVERY = (By.CSS_SELECTOR, '.AddressesCheckout-module__addressesCheckout--xlmQ9 button')
     LIST_STORES = (By.CSS_SELECTOR, '.Tabs-module__tabs--jbcOV > div:nth-child(2)')
     BUTTON_SELECT_PVZ = (By.CSS_SELECTOR, '.mf-offices-panel__pickups-list > a :nth-child(3) > .mf-offices-panel__item-select')
+    INPUT_ADDRESS_DELIVERY = (By.CSS_SELECTOR, '.Select-module__select--W-iMl input')
+    BUTTON_SAVE_ADDRESS_DELIVERY = (By.CSS_SELECTOR, 'button.ChangeAddress-module__button--FcrvC')
+    # ТИП ДОСТАВКИ
+    TYPE_DELIVERY_COURIER = (By.CSS_SELECTOR, '.Checkout-module__main--a-NUG > div:nth-child(2) > div > div:nth-child(6) > .OptionsWrapper-module__options--I4ac4 > div:nth-child(1)')
+    TYPE_DELIVERY_EXPRESS = (By.CSS_SELECTOR, '.Checkout-module__main--a-NUG > div:nth-child(2) > div > div:nth-child(6) > .OptionsWrapper-module__options--I4ac4 > div:nth-child(2)')
+    # ДАТА И ВРЕМЯ ДОСТАВКИ
+    SPAN_DATE_DELIVERY = (By.CSS_SELECTOR, '.DeliverySlotsWrapper-module__list--zwv0l > div:nth-child(1) span')
     # СПОСОБ ОПЛАТЫ
     PAYMENT_METHOD_CASH = (By.CSS_SELECTOR, '.Checkout-module__main--a-NUG > div:nth-child(2) > div > div:nth-child(6) > .OptionsWrapper-module__options--I4ac4 > div:nth-child(2)')
+    PAYMENT_METHOD_CASH_FOR_GET_METHOD_DELIVERY = (By.CSS_SELECTOR, '.Checkout-module__main--a-NUG > div:nth-child(2) > div > div:nth-child(8) > .OptionsWrapper-module__options--I4ac4 > div:nth-child(1)')
     # ОФОРМИТЬ ЗАКАЗ
     BUTTON_CHECKOUT = (By.CSS_SELECTOR, '.FixedSummary-module__main--uwywf button')
     INPUT_CONFIRM_SMS_CODE = (By.CSS_SELECTOR, '.ConfirmSmsCode-module__confirmSmsCode--2klTl input')
