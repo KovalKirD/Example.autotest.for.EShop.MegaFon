22ccb2d228ab86dc9e7297c777da80ed33eebdd1
Kirill <kovalev.kir.dmitr@gmail.com>
2023-08-29 16:49:22 +0300

    create exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
 # Базовая Страница
 from selenium.webdriver.support.expected_conditions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):  # конструктор класса
@@ -13,6 +15,13 @@ class BasePage:
     def is_element_selected(self, how, what):  # элемент должен быть выбран
         try:
             self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def is_element_present(self, how, what):  # элемент присутвтует на странице
+        try:
+            WebDriverWait(self.browser, 7).until(EC.presence_of_element_located((how, what)))
         except NoSuchElementException:
             return False
         return True
