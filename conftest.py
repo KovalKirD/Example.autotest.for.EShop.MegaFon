a03d9ae495a9e2f9bc6193dfd8164c60e77a2ef0
KovalKirD <kovalev.kir.dmitr@gmail.com>
2024-03-03 12:18:41 +0300

    add option

--- conftest.py
+++ conftest.py
@@ -2,11 +2,23 @@
 import pytest                   # пакет pytest
 from selenium import webdriver  # пакет Вебдрайвер
 
+def pytest_addoption(parser):  # обработчик опции
+    parser.addoption('--browser_name', action='store', default='firefox',
+                     help='Choose browser: chrome or firefox')
+
 # фикстура запуска/закрытия браузера
 @pytest.fixture(scope='function')
-def browser():
-    browser = webdriver.Firefox()  # инициализируем драйвер браузера
-    print('\nbrowser start for test')
+def browser(request):
+    browser_name = request.config.getoption("browser_name")  #запрос опции в cli
+    browser = None
+    if browser_name == "chrome":  # инициализируем драйвер браузера
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser                 # финализатор
     print('\nbrowser quit')
     browser.quit()                # закрыть браузер
