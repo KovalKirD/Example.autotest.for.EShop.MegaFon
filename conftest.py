eabb59daa84f5bd12f4c6600bb63416c908ef4c6
KovalKirD <kovalev.kir.dmitr@gmail.com>
2024-03-28 12:55:26 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 # Конфигурация Тестов
 import pytest                   # пакет pytest
 from selenium import webdriver  # пакет Вебдрайвер
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as opt_frfx
+from selenium.webdriver.chrome.options import Options as opt_chrm
 
 def pytest_addoption(parser):  # обработчик опции
     parser.addoption('--browser_name', action='store', default='firefox',
@@ -15,15 +15,17 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")  #запрос опции в cli
     browser = None
     ui = request.config.getoption("ui")
-    option = Options()
+    option_fx = opt_frfx()
+    option_ch = opt_chrm()
     if ui == "headless":
-        option.add_argument('--headless')
+        option_fx.add_argument('--headless')
+        option_ch.add_argument('--headless')
     if browser_name == "chrome":  # инициализируем драйвер браузера
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=option)
+        browser = webdriver.Chrome(options=option_ch)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(options=option)
+        browser = webdriver.Firefox(options=option_fx)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser                 # финализатор
